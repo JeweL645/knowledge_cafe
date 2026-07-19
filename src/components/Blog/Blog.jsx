2128e31519ad3ab4ecf78e81f2b99826036bf49c
JeweL645 <eliasislam645@gmail.com>
2026-07-19 14:02:24 +0600

    worked on the layout

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,15 +1,41 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Blog = ({blog}) => {
-    console.log(blog);
+const Blog = ({ blog }) => {
+  const {
+    title,
+    coverImage,
+    reading_time,
+    author_img,
+    author,
+    posted_date,
+    hashtag,
+  } = blog;
   return (
     <div>
-      
+      <img src={coverImage} alt={title} />
+      <div className="flex justify-between items-center my-4">
+        <div className="flex">
+          <img className="w-14 mr-6" src={author_img} />
+          <div>
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+
+      <h2 className="text-4xl">{title}</h2>
+      <p>
+        {
+          hashtag.map( (hash, idx)  => <span key={idx}> <a href="">#{hash}</a> </span>)
+        }</p>
     </div>
   );
 };
 
 Blog.propType = {
-    blog: PropTypes.object.isRequired
-}
-export default Blog
+  blog: PropTypes.object.isRequired,
+};
+export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -12,6 +13,13 @@ const Blogs = () => {
   return (
     <div className="md:w-2/3">
       <h2>Blogs: {blogs.length}</h2>
+      {
+      blogs.map(blog => <Blog 
+        key={blog.id}
+        blog={blog}
+        ></Blog>
+      )
+      }
     </div>
   );
 };
